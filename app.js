1d8659a4b15a51b1bb175d28d9d9173346e452ca
mgeorgievsky <max.georgievsky@gmail.com>
2022-01-10 17:26:40 +0300

    added socket.io to server

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const config = require('config')
 const express = require('express')
 const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+const cookie = require('cookie')
+const jwt = require('jsonwebtoken')
+const User = require('./models/User')
+
+const config = require('config')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+
 const path = require('path')
 const serveStatic = require('serve-static')
 
-const PORT = config.get("port") || 5000
 
-mongoose.set('useFindAndModify', false);
+
+const PORT = config.get('port') || 5000
+
+mongoose.set('useFindAndModify', false)
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
@@ -25,19 +37,53 @@ app.use('/api/auth/', require('./routes/auth.route'))
 app.use('/api/profile/', require('./routes/profile.route'))
 app.use('/api/users/', require('./routes/users.route'))
 app.use('/api/posts/', require('./routes/posts.route'))
+app.get('/testsocket', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+// want somehow to separate io to another file
+io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.request.headers['cookie'])
+    const {accessToken} = cookies
+    console.log('Access token: ' + accessToken)
+
+    if (!accessToken ) return next(new Error("not authorized"))
+
+    try {
+        const {userId} = await jwt.verify(accessToken, config.get('jwtSecret'))
+        socket.user = await User.findById(userId)
+        return next()
+    } catch(e) {
+        if (e instanceof jwt.JsonWebTokenError) console.log("Invalid access token")
+        if (e instanceof jwt.TokenExpiredError) console.log("Expired access token")
+        return next(new Error("not authorized"))
+    }
+
+    return next()
+})
+
+io.on('connection', (socket) => {
+    console.log(`${socket.user.username} connected`)
+    socket.on('disconnect', () => console.log(`${socket.user.username} disconnected`))
+
+    socket.on('message', (message) => {
+        console.log(`Message from ${socket.user.username}: ${message}`)
+    })
+})
+
 
 const start = async () => {
     try {
         await mongoose.connect(
-            config.get("databaseURI"),
+            config.get('databaseURI'),
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
                 useCreateIndex: true
             }
         )
-        app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
-    } catch(e) {
+        server.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`)
+        })
+    } catch (e) {
         console.log(e)
     }
 }
