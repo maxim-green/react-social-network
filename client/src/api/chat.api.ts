4ed274f59a895785883005c20cf3604550969199
mgeorgievsky <max.georgievsky@gmail.com>
2022-01-16 13:20:40 +0300

    trying to figure out bug in dialogs.

--- client/src/api/chat.api.ts
+++ client/src/api/chat.api.ts
@@ -6,11 +6,14 @@ let subscribers = [] as Array<(message: MessageType) => void>
 let socket = io('http://localhost:5000', {withCredentials: true, autoConnect: false})
 
 const handleServerMessage = (message: MessageType) => {
-    console.log('Message from server ' + message)
+    console.log('Message from server ', message)
     subscribers.forEach(s => s(message))
 }
 
+socket.on('server-message', handleServerMessage)
+
 // todo need to debug: after logout and then login again several messages show up on UI (on message sent)
+// unsubscribe isnt working (but its called). Subscribers array not changing after logout.
 export const chatApi = {
     connect() {
         if (!socket.connected) socket.connect()
@@ -21,16 +24,16 @@ export const chatApi = {
         console.log('Socket connection closed')
     },
     subscribe(callback: (message: MessageType) => void) {
-        socket.on('server-message', handleServerMessage)
+
         subscribers.push(callback)
         return () => {
             subscribers = subscribers.filter(s => s != callback)
         }
     },
     unsubscribe(callback: (message: MessageType) => void) {
-        return () => {
+        console.log('before', subscribers)
             subscribers = subscribers.filter(s => s != callback)
-        }
+        console.log('after', subscribers)
     },
 
     sendMessage(message: string) {
