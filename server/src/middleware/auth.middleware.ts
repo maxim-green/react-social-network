3c6af9e773ec7a2173bca22c210a12458a42b722
mgeorgievsky <max.georgievsky@gmail.com>
2022-02-12 13:24:42 +0300

    refactored auth.middleware.ts

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -3,67 +3,78 @@ import config from 'config'
 import {Error} from 'mongoose'
 
 import {User} from 'models'
-import {Request, Response, NextFunction, JwtPayloadWithUserId, UserType} from 'types'
+import {Request, Response, NextFunction, UserType} from 'types'
+
+
+
+const verifyToken = <T extends object>(token: string) => {
+    return jwt.verify(token, config.get('jwtSecret')) as T
+}
+
+const getUserByRefreshToken = async (refreshToken: string) => {
+    const { userId } = verifyToken<{ userId: string }>(refreshToken)    // validating RT
+
+    const candidate = await User.findById(userId)
+        .select('-password')
+        .populate<{ subscriptions: Array<UserType> }>('subscriptions', 'username firstName lastName avatar subscriptions')
+
+    if (!candidate) throw new Error('Invalid token')
+
+
+    if (!(refreshToken === candidate.refreshToken)) {   // comparing provided RT with RT from database
+        candidate.refreshToken = ''
+        await candidate.save()
+        throw new Error('Invalid token')
+    }
+
+    return candidate    // RT valid and matching RT from database
+}
+
+const getUserByAccessToken = async (accessToken: string) => {
+    const { userId } = verifyToken<{ userId: string }>(accessToken)    // validating RT
+
+    const user = await User.findById(userId)
+        .select('-refreshToken -password')
+        .populate<{ subscriptions: Array<UserType> }>('subscriptions', 'username firstName lastName avatar subscriptions')
+
+    if (!user) throw new Error('Invalid token')
+
+    return user
+}
+
+
 
 export const defineUserByRefreshToken = async (req: Request, res: Response, next: NextFunction) => {
-    if (req.method === 'OPTIONS') {
-        return next()
-    }
-
-    const {refreshToken} = req.cookies
-    if (!refreshToken) return next()
-
     try {
-        await jwt.verify(
-            refreshToken,
-            config.get('jwtSecret'),
-            async (err: Error, payload: JwtPayloadWithUserId) => {
-                if (err) return next(new Error('Not authorized'))
 
-                const candidate = await User.findById(payload.userId, 'refreshToken')
+        if (req.method === 'OPTIONS') return next()
 
-                if (!(refreshToken === candidate.refreshToken)) {
-                    candidate.refreshToken = ''
-                    await candidate.save()
-                    return res.status(401).json({resultCode: 1, message: 'Invalid token'})
-                }
+        if (!req.cookies.refreshToken) return next()
 
-                req.user = await User.findById(payload.userId)
-                    .select('-refreshToken -password')
-                    .populate<{ subscriptions: Array<UserType> }>('subscriptions', 'username firstName lastName avatar subscriptions')
-            })
+        req.user = await getUserByRefreshToken(req.cookies.refreshToken)
 
         return next()
+
     } catch (e) {
+
         if (e instanceof jwt.JsonWebTokenError) console.log('Invalid refresh token')
         if (e instanceof jwt.TokenExpiredError) console.log('Expired refresh token')
-    }
+        next()
 
-    return next()
+    }
 }
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
-    if (req.method === 'OPTIONS') {
-        return next()
-    }
-
-    const {accessToken} = req.cookies
-    if (!accessToken) return next()
-
     try {
-        await jwt.verify(
-            accessToken,
-            config.get('jwtSecret'),
-            async (err: Error, payload: JwtPayloadWithUserId) => {
-                if (err) {
-                    return next()
-                }
 
-                req.user = await User.findById(payload.userId)
-                    .select('-refreshToken -password')
-                    .populate<{ subscriptions: Array<UserType> }>('subscriptions', 'username firstName lastName avatar subscriptions')
-                return next()
-            })
+        if (req.method === 'OPTIONS') return next()
+
+        if (!req.cookies.accessToken) return next()
+
+        req.user = await getUserByAccessToken(req.cookies.accessToken)
+
+        return next()
+
     } catch (e) {
         if (e instanceof jwt.JsonWebTokenError) console.log('Invalid access token')
         if (e instanceof jwt.TokenExpiredError) console.log('Expired access token')
@@ -71,10 +82,14 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
 }
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    const {user} = req
-    if (!user) {
-        return res.status(401).json({resultCode: 1, message: 'Not authorized'})
-    } else {
-        return next()
+    try {
+        const {user} = req
+        if (!user) {
+            return res.status(401).json({resultCode: 1, message: 'Not authorized'})
+        } else {
+            return next()
+        }
+    } catch (e) {
+        return res.status(500).json({resultCode: 1, message: 'Something went wrong :('})
     }
 }
