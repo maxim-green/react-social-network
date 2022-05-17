8ecab1e34a959b1ad73c28655b626fdfd2c2d3fb
mgeorgievsky <max.georgievsky@gmail.com>
2022-05-17 15:32:55 +0300

    changed Access-Control-Allow-Origin header to *

--- server/src/configs/express.config.ts
+++ server/src/configs/express.config.ts
@@ -11,7 +11,7 @@ import {withErrorHandler} from 'middleware'
 const expressApp = express()
 
 expressApp.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'content-type')
     res.setHeader('Access-Control-Allow-Credentials', 'true')
